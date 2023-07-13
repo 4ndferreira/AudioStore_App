d5385e354fe98bd54f416708b4f17880384533c5
4ndferreira <andferreira.art@outlook.com>
2023-07-12 23:26:55 -0300

    Feat: making the API request

--- my-challenge-app/src/App.tsx
+++ my-challenge-app/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
 
-import Auth from './pages/Auth'
-import Home from './pages/Home'
+import Auth from './pages/auth/Auth'
+import Home from './pages/home/Home'
 import Search from './pages/Search'
-import Products from './pages/Products'
-import Product from './pages/Product'
-import ShoppingCart from './pages/ShoppingCart'
+import Products from './pages/products/Products'
+import Product from './pages/product/Product'
+import ShoppingCart from './pages/shoppingCart/ShoppingCart'
 
 import './App.css'
 
 const App = () => {
+
+  axios
+    .get("https://run.mocky.io/v3/58d95159-6053-48c1-9351-32a1b1325a4f")
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
 
   return (
     <BrowserRouter>
