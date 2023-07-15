55224276eb81a976bcca8c6ae3757f40861fbbf9
4ndferreira <andferreira.art@outlook.com>
2023-07-15 13:19:45 -0300

    Fix: Counter component added

--- my-challenge-app/src/components/searchItem/SearchItem.tsx
+++ my-challenge-app/src/components/searchItem/SearchItem.tsx
@@ -1,19 +1,26 @@
 import LinkViewMore from '../card/LinkViewMore';
+import Counter from '../counter/Counter';
 import classes from './SearchItem.module.css'
 
-const SearchItem = () => {
+const SearchItem = (props: {isShoppingCart: boolean}) => {
   return (
     <div className={classes.container}>
       <div className={classes.itemImageContainer}>
-        <img src='/img/image5.png' alt='' />
+        <img src="/img/image5.png" alt="" />
       </div>
       <div className={classes.itemTextContainer}>
         <h4>TMA-2 Comfort Wireless</h4>
         <ul>
           <li>USD 270</li>
-          <li className={classes.itemReview}>
-            <LinkViewMore />
-          </li>
+          {props.isShoppingCart ? (
+            <li>
+              <Counter />
+            </li>
+          ) : (
+            <li className={classes.itemReview}>
+              <LinkViewMore />
+            </li>
+          )}
         </ul>
       </div>
     </div>
