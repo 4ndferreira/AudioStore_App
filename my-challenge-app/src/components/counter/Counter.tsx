5b8416fdd040a9a4d4feda9d33d52ea32721554f
4ndferreira <andferreira.art@outlook.com>
2023-07-15 13:23:49 -0300

    Feat: added delete button

--- my-challenge-app/src/components/counter/Counter.tsx
+++ my-challenge-app/src/components/counter/Counter.tsx
@@ -1,33 +1,39 @@
 import { useState } from "react";
 import IconMinus from "./IconMinus";
 import IconPlus from "./IconPlus";
+import IconTrash from "../navBar/IconTrash";
 import classes from './Counter.module.css'
 
 const Count = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   const handleIncrease = () => {
     setCount(count => count + 1)
   }
   const handleDecrease = () => {
-    count > 0 && setCount(count => count - 1)
+    count > 1 && setCount(count => count - 1)
   }
 
   return (
     <div className={classes.wrapper}>
-      <p
-        className={classes.button} 
-        onClick={handleDecrease}
-      >
-        <IconMinus />
-      </p>
-      <p>{count}</p>
-      <p
-        className={classes.button}
-        onClick={handleIncrease}
-      > 
-        <IconPlus />
-      </p>
+      <div className={classes.counter}>
+        <p
+          className={classes.button} 
+          onClick={handleDecrease}
+        >
+          <IconMinus />
+        </p>
+        <p>{count}</p>
+        <p
+          className={classes.button}
+          onClick={handleIncrease}
+        > 
+          <IconPlus />
+        </p>
+      </div>
+        <p onClick={() => setCount(0)}>
+          <IconTrash />
+        </p>
     </div>
   );
 }
