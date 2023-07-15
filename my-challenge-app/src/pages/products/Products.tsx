496f4b3bdf1ee66625b918cd5ba210dd2f9bbf29
4ndferreira <andferreira.art@outlook.com>
2023-07-15 18:58:03 -0300

    Feat: Product page development

--- my-challenge-app/src/pages/products/Products.tsx
+++ my-challenge-app/src/pages/products/Products.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react"
+import Button from "../../components/button/Button"
+import classes from "./Products.module.css"
+import NavBar from "../../components/navBar/NavBar"
+import IconSliders from "../../components/button/IconSliders"
+
 const Products = () => {
+  const [isActive, setIsActive] = useState(false)
+  const activeFilter = () => {
+    setIsActive(!isActive)
+  }
+  
   return (
-    <div>Products</div>
-  )
+    <div className={classes.container}>
+      <NavBar />
+      <h2 className={classes.title}>
+        <p className={classes.titleSmall}>
+          Featured products
+        </p>
+        <p className={classes.titleBig}>
+          See all Products
+        </p>
+      </h2>
+      <Button 
+        type="button" 
+        name={<IconSliders />} 
+        onClick={activeFilter} 
+      />
+    </div>
+  );
 }
 
 export default Products
