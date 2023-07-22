1ae8b1aac8909f066d20575654ee378bf61ac43d
4ndferreira <andferreira.art@outlook.com>
2023-07-22 10:08:21 -0300

    Feat: component created

---
+++ my-challenge-app/src/components/loader/Loader.tsx
@@ -0,0 +1,11 @@
+import classes from './Loader.module.css'
+
+const Loader = () => {
+  return (
+    <div className={classes.loaderWrapper}>
+      <p className={classes.loader}></p>
+    </div>
+  );
+}
+
+export default Loader
