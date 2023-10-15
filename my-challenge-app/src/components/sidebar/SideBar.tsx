65728234fcf3373a32e7ab7efd30edeb524d3f88
4ndferreira <andferreira.art@outlook.com>
2023-10-15 14:08:26 -0300

    Feat: added link to search page

--- my-challenge-app/src/components/sidebar/SideBar.tsx
+++ my-challenge-app/src/components/sidebar/SideBar.tsx
@@ -9,6 +9,7 @@ import GridIcon from "./GridIcon";
 //CSS
 import classes from "./Sidebar.module.css";
 import { motion } from "framer-motion";
+import SearchIcon from "../labelInput/SearchIcon";
 
 const SideBar = (props: { onClick: MouseEventHandler<HTMLButtonElement> }) => {
   const { cartItemCount } = useContext(CartContext);
@@ -16,27 +17,51 @@ const SideBar = (props: { onClick: MouseEventHandler<HTMLButtonElement> }) => {
     <div className={classes.page}>
       <motion.nav
         initial={{ width: 0 }}
-        animate={{ width: "50%", transition: { duration: 0.3 } }}
+        animate={{ width: "70%", transition: { duration: 0.3 } }}
         exit={{ x: 0 }}
         className={classes.sidebar}
       >
-        <NavLink className={classes.menuLink} to={"/products"}>
-          <GridIcon />
-          <span>Products</span>
-        </NavLink>
-        <NavLink className={classes.menuLink} to={"/cart"}>
-          <span className={classes.iconCart}>
-            <IconShoppingCart />
-            <div className={classes.cartControl}>
-              <p>{cartItemCount}</p>
-            </div>
-          </span>
-          <span>Cart</span>
-        </NavLink>
-        <button className={classes.buttonLogout} onClick={props.onClick}>
+        <ul className={classes.menuList}>
+        <li>
+            <NavLink 
+              className={classes.menuLink} 
+              to={"/products"}
+            >
+              <SearchIcon size={"25"} color={"black"} />
+              <span>Search</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              className={classes.menuLink} 
+              to={"/products"}
+            >
+              <GridIcon />
+              <span>Products</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              className={classes.menuLink} 
+              to={"/cart"}
+            >
+              <span className={classes.iconCart}>
+                <IconShoppingCart />
+                <div className={classes.cartControl}>
+                  <p>{cartItemCount}</p>
+                </div>
+              </span>
+              <span>Cart</span>
+            </NavLink>
+          </li>
+        </ul>
+        <button 
+          className={classes.buttonLogout} 
+          onClick={props.onClick}
+        >
           <IconLogout />
           <span>Logout</span>
-        </button>
+        </button>      
       </motion.nav>
       <motion.div
         initial={{ opacity: 0 }}
