c38838d0a83a50525ea2709738536bfce6db982f
4ndferreira <andferreira.art@outlook.com>
2023-10-27 11:02:41 -0300

    Feat: separating mobile and desktop routes

--- my-challenge-app/src/App.tsx
+++ my-challenge-app/src/App.tsx
@@ -2,13 +2,15 @@
 import { lazy, useEffect } from 'react'
 //React Router DOM
 import { BrowserRouter, Route, useLocation } from 'react-router-dom'
+//Hook
+import { useMediaQuery } from './hooks/useMediaQuery'
 //Components
 import CartProvider from './store/CartProvider'
 import PrivateWrapper from './layouts/PrivateWrapper'
-//CSS
-import './App.css'
+import MobileExclusiveRoutes from './layouts/MobileExclusiveRoutes'
 import AnimatedRoutes from './layouts/AnimatedRoutes'
 //Lazy Components
+const Welcome = lazy(() => import('./pages/welcome/Welcome'))
 const Login = lazy(() => import('./pages/login/Login'))
 const Home = lazy(() => import('./pages/home/Home'))
 const Search = lazy(() => import('./pages/search/Search'))
@@ -16,6 +18,8 @@ const Products = lazy(() => import('./pages/products/Products'))
 const Product = lazy(() => import('./pages/product/Product'))
 const ShoppingCart = lazy(() => import('./pages/shoppingCart/ShoppingCart'))
 const NotFound = lazy(()=> import('./pages/notFound/NotFound'))
+//CSS
+import './App.css'
 
 export const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -33,49 +37,27 @@ export const ChangeOverFlow = () => {
   return null;
 };
 
-const App = () => {
+export default function App() {
+  const isDesktop = useMediaQuery();
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      <ChangeOverFlow />
       <CartProvider>
-        <ScrollToTop />
-        <ChangeOverFlow />
         <AnimatedRoutes>
-          <Route 
-            path='/signin' 
-            element={<Login />} 
-          />
-          <Route 
-            element={<PrivateWrapper />}
-          >
-            <Route 
-              path='/'
-              element={<Home />} 
-            />
-            <Route 
-              path='/search' 
-              element={<Search />} 
-            />
-            <Route 
-              path='/products' 
-              element={<Products />} 
-            />
-            <Route 
-              path='/products/:id' 
-              element={<Product />} 
-            />
-            <Route 
-              path='/cart' 
-              element={<ShoppingCart />} 
-            />
-            <Route 
-              path='*' 
-              element={<NotFound />} 
-            />
+          <Route element={<PrivateWrapper />}>
+            <Route path="/" element={isDesktop ? <Welcome /> : <Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route element={<MobileExclusiveRoutes />}>
+              <Route path="/search" element={<Search />} />
+              <Route path="/products/:id" element={<Product />} />
+              <Route path="/cart" element={<ShoppingCart />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
         </AnimatedRoutes>
       </CartProvider>
     </BrowserRouter>
-  )
+  );
 }
-
-export default App
