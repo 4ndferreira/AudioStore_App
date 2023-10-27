81e179f3c0bf2bb9002199fc1356347440b19a21
4ndferreira <andferreira.art@outlook.com>
2023-10-27 10:54:29 -0300

    Feat: route animation for mobile version only

--- my-challenge-app/src/layouts/AnimatedRoutes.tsx
+++ my-challenge-app/src/layouts/AnimatedRoutes.tsx
@@ -3,19 +3,29 @@ import { AnimatePresence } from 'framer-motion'
 import { Routes, useLocation } from 'react-router-dom'
 import Loader from '../components/loader/Loader'
 import Page from './Page'
+import { useMediaQuery } from '../hooks/useMediaQuery'
 
 export default function AnimatedRoutes(props: { children: React.ReactNode }) {
+  const isDesktop = useMediaQuery();
   const location = useLocation()
 
   return (
-    <AnimatePresence initial={false}>
-      <Page key={location.key}>
-        <Suspense fallback={<Loader />}>
-          <Routes location={location} key={location.pathname}>
-            {props.children}
-          </Routes>
-        </Suspense>
-      </Page>
-    </AnimatePresence>
+    !isDesktop ? (
+      <AnimatePresence initial={false}>
+        <Page key={location.key}>
+          <Suspense fallback={<Loader />}>
+            <Routes location={location} key={location.key}>
+              {props.children}
+            </Routes>
+          </Suspense>
+        </Page>
+      </AnimatePresence>
+    ) : (
+      <Suspense fallback={<Loader />}>
+        <Routes location={location} key={location.key}>
+          {props.children}
+        </Routes>
+      </Suspense>
+    )
   )
 }
