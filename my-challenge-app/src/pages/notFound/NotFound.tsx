5618c14d681bc24dee69d16fa68e6740b6a088ce
4ndferreira <andferreira.art@outlook.com>
2023-10-27 11:36:28 -0300

    Refactor: changed return path

--- my-challenge-app/src/pages/notFound/NotFound.tsx
+++ my-challenge-app/src/pages/notFound/NotFound.tsx
@@ -1,17 +1,18 @@
+// React Router
 import { Link } from 'react-router-dom';
+//Icon
 import IconAlert from '../../components/icons/IconAlert';
+//CSS
 import classes from './NotFound.module.css'
 
-const NotFound = () => {
+export default function NotFound() {
   return (
     <div className={classes.notFoundMessage}>
       <IconAlert width={"96"} height={"96"} />
       <h1 className={classes.notFoundText}>404 - Page Not Found</h1>
-      <Link to={"/"} state={{ isPush: true }} className={classes.notFoundLink}>
+      <Link to={"/home"} state={{ isPush: true }} className={classes.notFoundLink}>
         Get me out of here!
       </Link>
     </div>
   );
 }
-
-export default NotFound
