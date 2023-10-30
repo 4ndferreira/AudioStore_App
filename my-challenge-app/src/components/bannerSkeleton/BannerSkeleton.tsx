f95ae9ccf7f78c18b73b3f1629fa42b3b1c91340
4ndferreira <andferreira.art@outlook.com>
2023-10-29 21:03:05 -0300

    Feat: adjustments for desktop version

--- my-challenge-app/src/components/bannerSkeleton/BannerSkeleton.tsx
+++ my-challenge-app/src/components/bannerSkeleton/BannerSkeleton.tsx
@@ -1,25 +1,30 @@
 //React loading Skeleton
 import Skeleton, { SkeletonTheme} from "react-loading-skeleton"
-//Skeleton
+//Hook
+import { useMediaQuery } from "../../hooks/useMediaQuery";
+//CSS
 import 'react-loading-skeleton/dist/skeleton.css'
 import classes from './BannerSkeleton.module.css'
 
-const BannerSkeleton = () => {
+export default function BannerSkeleton() {
+  const isDesktop = useMediaQuery();
+  
   return (
     <SkeletonTheme>
       <div className={classes.container}>
         <div className={classes.textSkeleton}>
           <Skeleton
             count={3}
-            height={"1.375rem"}
+            height={isDesktop? "1.65rem" : "1.375rem"}
             style={{ marginBottom: "0.5rem" }}
           />
           <Skeleton count={1} width={"60%"} height={"1rem"} />
         </div>
-        <Skeleton width={"134.05px"} height={"144.63px"} />
+        <Skeleton 
+          width={isDesktop? "163.125px" : "134.05px"} 
+          height={isDesktop? "176px" : "144.63px"} 
+        />
       </div>
     </SkeletonTheme>
   );
-};
-
-export default BannerSkeleton
+}
