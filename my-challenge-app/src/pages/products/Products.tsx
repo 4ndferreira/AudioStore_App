60638b4086bb2f6f0bc1ccaafd3c8f85830dd592
4ndferreira <andferreira.art@outlook.com>
2023-11-01 18:02:27 -0300

    Feat: creating desktop version

--- my-challenge-app/src/pages/products/Products.tsx
+++ my-challenge-app/src/pages/products/Products.tsx
@@ -1,183 +1,179 @@
 //React
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState } from "react";
 //Hook
-import { Data, useFetch } from '../../hooks/useFetch'
+import { Data, useFetch } from "../../hooks/useFetch";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 //React Spring Bottom Sheet
-import { BottomSheet } from 'react-spring-bottom-sheet-updated'
+import { BottomSheet } from "react-spring-bottom-sheet-updated";
 //Components
-import Button from '../../components/button/Button'
-import NavBar from '../../components/navBar/NavBar'
-import Card from '../../components/card/Card'
-import Categories from '../../components/categories/Categories'
-import SortBy from '../../components/sortBy/SortBy'
-import CardSkeleton from '../../components/cardSkeleton/CardSkeleton'
+import Button from "../../components/button/Button";
+import HeaderMobileWhileBrowsing from "../../components/headerMobileWhileBrowsing/HeaderMobileWhileBrowsing";
+import CardSkeleton from "../../components/cardSkeleton/CardSkeleton";
+import LoadingError from "../../components/loadingError/LoadingError";
 //Icons
-import IconSliders from '../../components/icons/IconSliders'
-import IconClose from '../../components/icons/IconClose'
-import IconShoppingBag from '../../components/icons/IconShoppingBag'
+import IconSliders from "../../components/icons/IconSliders";
+import IconShoppingBag from "../../components/icons/IconShoppingBag";
 //CSS
-import classes from './Products.module.css'
-import './style.css'
+import classes from "./Products.module.css";
+import "./style.css";
+import ShowAllCardsOfProducts from "../../components/showAllCardsOfProducts/ShowAllCardsOfProducts";
+import PagesWithAllProducts from "../../components/pagesWithAllProducts/PagesWithAllProducts";
+import AllFilteringOptions from "../../components/allFilteringOptions/AllFilteringOptions";
+import Modal from "../../components/modal/Modal";
 
-export default function Products() { 
-  const [open, setOpen] = useState(false) 
-  const [ filterCategory, setFilterCategory] = useState ('')
-  const [ selectSortBy, setSelectSortBy] = useState ('')
-  const [ sortedData, setSortedData] = useState<Data[] | undefined> (undefined)
+export default function Products() {
+  const [open, setOpen] = useState(false);
+  const [filterCategory, setFilterCategory] = useState("");
+  const [selectSortBy, setSelectSortBy] = useState("");
+  const [sortedData, setSortedData] = useState<Data[] | undefined>(undefined);
 
   const { data, loading, error } = useFetch();
-  
+
+  const isDesktop = useMediaQuery();
+
   const openFilter = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
   const handleCloseFilter = (e: { preventDefault: () => void }) => {
-    e.preventDefault()
-    setOpen(false)
-  }
+    e.preventDefault();
+    setOpen(false);
+  };
 
   const handleSelectCategory = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target
-    setFilterCategory(value)
-  }
+    const { value } = e.target;
+    setFilterCategory(value);
+  };
 
   const handleSelectSortBy = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target
-    setSelectSortBy(value)
-  }
-  
+    const { value } = e.target;
+    setSelectSortBy(value);
+  };
+
   const handleSortBy = (e: { preventDefault: () => void }) => {
-    e.preventDefault()
-    const filteredData = data?.filter((item) => 
+    e.preventDefault();
+    const filteredData = data?.filter((item) =>
       item.category.includes(filterCategory)
     );
-    
-    selectSortBy === 'Popularity' &&
-    filteredData?.sort((a, b) => b.reviews.length - a.reviews.length);
 
-    selectSortBy === 'Newest' &&
-    filteredData?.sort(
+    selectSortBy === "Popularity" &&
+      filteredData?.sort((a, b) => b.reviews.length - a.reviews.length);
+
+    selectSortBy === "Newest" &&
+      filteredData?.sort(
         (a, b) =>
           new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
       );
 
-    selectSortBy === 'Oldest' &&
-    filteredData?.sort(
+    selectSortBy === "Oldest" &&
+      filteredData?.sort(
         (a, b) =>
           new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
       );
 
-    selectSortBy === 'High Price' &&
-    filteredData?.sort(
+    selectSortBy === "High Price" &&
+      filteredData?.sort(
         (a, b) =>
           parseFloat(b.price.replace("$", "")) -
           parseFloat(a.price.replace("$", ""))
       );
 
-    selectSortBy === 'Low Price' &&
-    filteredData?.sort(
+    selectSortBy === "Low Price" &&
+      filteredData?.sort(
         (a, b) =>
           parseFloat(a.price.replace("$", "")) -
           parseFloat(b.price.replace("$", ""))
       );
 
-    selectSortBy === 'Review' && filteredData?.sort((a, b) => b.rating - a.rating);
-    
-    setSortedData(filteredData)
-    setOpen(!open)
-  }
+    selectSortBy === "Review" &&
+      filteredData?.sort((a, b) => b.rating - a.rating);
+
+    setSortedData(filteredData);
+    setOpen(!open);
+  };
 
   return (
     <>
-      <div className={classes.containerTop}>
-        <NavBar
-          link="/home"
-          title={""}
-          isShoppingCart={false}
-        />
-        <h2 className={classes.title}>
-          <p className={classes.titleSmall}>Featured products</p>
-          <p className={classes.titleBig}>See all Products</p>
-        </h2>
-      </div>
-      <div className={classes.wrapper}>
-        <Button type="button" onClick={openFilter}>
-          <IconSliders />
-        </Button>
-      </div>
-      <section className={classes.showcase}>
-        <div className={classes.itemsContainer}>
-          {!data || loading ? (
-            <CardSkeleton cards={6} />
-            ) : error ? (
-              <p>{error}</p>
-            ) : sortedData ? (
-            sortedData.length !== 0 ? (
-              sortedData.map((item) => (
-                <Card
-                  id={item.id}
-                  key={item.id}
-                  name={item.name}
-                  price={item.price}
-                  rating={item.rating}
-                  showReview={true} 
-                  category={item.category}                
-                />
-              ))
+      {error ? (
+        <LoadingError />
+      ) : (
+        <div className={classes.container}>
+          {!isDesktop && <HeaderMobileWhileBrowsing/>}
+          <section className={classes.wrapper}>
+            <div className={classes.title}>
+              <h3 className={classes.titleSmall}>Featured products</h3>
+              <h2 className={classes.titleBig}>See all Products</h2>
+            </div>
+            <Button type="button" onClick={openFilter}>
+              <IconSliders />
+            </Button>
+          </section>
+          <section className={classes.showcase}>
+            {isDesktop ? (
+              !data || loading ? (
+                <div className={classes.containerSkeleton}>
+                  <CardSkeleton cards={10} />
+                </div>
+              ) : sortedData ? (
+                <PagesWithAllProducts sortedData={sortedData} />
+              ) : (
+                <PagesWithAllProducts sortedData={data} />
+              )
             ) : (
-              <li className={classes.noReturn}>
-                <IconShoppingBag width={"96"} height={"96"} color={"#BABABA"} />
-                <h2>
-                  Ops!
-                  <br /> No products found in this category.
-                </h2>
-              </li>
+              <div className={classes.itemsContainer}>
+                {!data || loading ? (
+                  <CardSkeleton cards={6} />
+                ) : sortedData ? (
+                  sortedData.length !== 0 ? (
+                    <ShowAllCardsOfProducts data={sortedData} />
+                  ) : (
+                    <li className={classes.noReturn}>
+                      <IconShoppingBag
+                        width={"96"}
+                        height={"96"}
+                        color={"#BABABA"}
+                      />
+                      <h2>
+                        Ops!
+                        <br /> No products found in this category.
+                      </h2>
+                    </li>
+                  )
+                ) : (
+                  <ShowAllCardsOfProducts data={data} />
+                )}
+              </div>
+            )}
+          </section>
+          {isDesktop ? (
+            open && (
+              <div className={classes.wrapperModal}>
+                <Modal onClose={() => setOpen(false)} isOpen={open}>
+                  <AllFilteringOptions
+                    handleCloseFilter={handleCloseFilter}
+                    filterCategory={filterCategory}
+                    handleSelectCategory={handleSelectCategory}
+                    selectSortBy={selectSortBy}
+                    handleSelectSortBy={handleSelectSortBy}
+                    handleSortBy={handleSortBy}
+                  />
+                </Modal>
+              </div>
             )
           ) : (
-            data?.map((item) => (
-              <Card
-                id={item.id}
-                key={item.id}
-                name={item.name}
-                price={item.price}
-                rating={item.rating}
-                showReview={true}
-                category={item.category}
+            <BottomSheet open={open}>
+              <AllFilteringOptions
+                handleCloseFilter={handleCloseFilter}
+                filterCategory={filterCategory}
+                handleSelectCategory={handleSelectCategory}
+                selectSortBy={selectSortBy}
+                handleSelectSortBy={handleSelectSortBy}
+                handleSortBy={handleSortBy}
               />
-            ))
+            </BottomSheet>
           )}
         </div>
-      </section>
-      <BottomSheet open={open}>
-        <form className={classes.filterContainer}>
-          <div className={classes.filterTitle}>
-            <h2>Filter</h2>
-            <button className={classes.closeButton} onClick={handleCloseFilter}>
-              <IconClose color={'black'} />
-            </button>
-          </div>
-          <div>
-            <h4>Category</h4>
-            <Categories
-              filterSelected={filterCategory}
-              onChange={handleSelectCategory}
-            />
-          </div>
-          <div>
-            <h4>Sort By</h4>
-            <SortBy
-              filterSelected={selectSortBy}
-              onChange={handleSelectSortBy}
-            />
-          </div>
-          <Button
-            type={"submit"}
-            onClick={handleSortBy}
-          >
-            Apply Filter
-          </Button>
-        </form>
-      </BottomSheet>
+      )}
     </>
   );
 }
