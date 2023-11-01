72fdf69607e43066fbed74b5ca37284a7b813bb9
4ndferreira <andferreira.art@outlook.com>
2023-11-01 18:20:15 -0300

    Refactor: handle fetch error

--- my-challenge-app/src/pages/home/Home.tsx
+++ my-challenge-app/src/pages/home/Home.tsx
@@ -1,46 +1,46 @@
 //React
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useState } from "react";
 //React Router Dom
-import { Link, useNavigate } from "react-router-dom"
-//Splide
-import { Splide, SplideSlide, SplideTrack } from '../../../node_modules/@splidejs/react-splide'
+import { useNavigate } from "react-router-dom";
 //Firebase
-import { signOut } from "firebase/auth"
-import { auth } from "../../firebase/Config"
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/Config";
 //Hook
-import { useFetch } from "../../hooks/useFetch"
+import { useFetch } from "../../hooks/useFetch";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 // Components
-import Header from "../../components/header/Header"
-import Categories from "../../components/categories/Categories"
-import Banner from "../../components/banner/Banner"
-import Card from "../../components/card/Card"
-import Input from "../../components/input/Input"
-import SearchIcon from "../../components/icons/SearchIcon"
-import IconShoppingBag from "../../components/icons/IconShoppingBag"
-import CardSkeleton from "../../components/cardSkeleton/CardSkeleton"
-import BannerSkeleton from "../../components/bannerSkeleton/BannerSkeleton"
+import HeaderMobileForHome from "../../components/headerMobileForHome/HeaderMobileForHome";
+import Categories from "../../components/listOfSelectorLabels/ListOfSelectorLabels";
+import Input from "../../components/input/Input";
+import Showcase from "../../components/showcase/Showcase";
+import Carousel from "../../components/carousel/Carousel";
+import BannerSkeleton from "../../components/bannerSkeleton/BannerSkeleton";
+import SearchIcon from "../../components/icons/SearchIcon";
+import LoadingError from "../../components/loadingError/LoadingError";
 // CSS
-import '../../../node_modules/@splidejs/react-splide/dist/css/splide.min.css'
-import classes from './Home.module.css'
+import classes from "./Home.module.css";
 
-const Home = () => {
+export default function Home() {
   const { data, loading, error } = useFetch();
-  const [filterCategory, setFilterCategory] = useState('')
-  const navigate = useNavigate()
+  const [filterCategory, setFilterCategory] = useState("");
+  const navigate = useNavigate();
+  const isDesktop = useMediaQuery();
 
   const handleSignOut = () => {
-    signOut(auth).then(() => {
-      console.log('Sign-out successful.')
-      navigate('/signin')
-    }).catch((error) => {
-      console.error(error.code)
-    });
-  }
+    signOut(auth)
+      .then(() => {
+        console.log("Sign-out successful.");
+        navigate("/login", { state: { isPush: true } });
+      })
+      .catch((error) => {
+        console.error(error.code);
+      });
+  };
 
   const handleSelectChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target
-    setFilterCategory(value)
-  }
+    const { value } = e.target;
+    setFilterCategory(value);
+  };
 
   const filteredData = data?.filter((item) =>
     item.category.includes(filterCategory)
@@ -48,136 +48,87 @@ const Home = () => {
 
   return (
     <>
-      <div className={classes.container}>
-        <div className={classes.headerContainer}>
-          <Header image={auth.currentUser?.photoURL} onClick={handleSignOut} />
-          <h3 className={classes.welcomeText}>
-            <small className={classes.welcomeTextSmall}>
-              Hi,{" "}
-              {auth.currentUser?.displayName
-                ? auth.currentUser?.displayName.split(" ")[0]
-                : auth.currentUser?.email?.split("@")[0]}
-            </small>
-            What are you looking for today?
-          </h3>
-        </div>
-        <Link to="/search" className={classes.wrapper}>
-          <Input
-            id={"searchBar"}
-            type={"text"}
-            name={"Search headphone"}
-            element={<SearchIcon size={"20"} color={"#BABABA"} />}
-            value={""}
-            onInput={undefined}
-            onFocus={undefined}
+      {error ? (
+        <LoadingError />
+      ) : (
+        <div className={classes.container}>
+          <HeaderMobileForHome
+            image={auth.currentUser?.photoURL}
+            onClick={handleSignOut}
           />
-        </Link>
-        <section className={classes.showcase}>
-          <div className={classes.showcaseContainer}>
-            <Categories
-              filterSelected={filterCategory}
-              onChange={handleSelectChange}
-            />
-            {!data || loading ? (
-              <BannerSkeleton />
-            ) : error ? (
-              <p>{error}</p>
-            ) : (
-              <Splide
-                hasTrack={false}
-                options={{
-                  width: "100%",
-                  autoWidth: true,
-                  arrows: false,
-                  pagination: false,
-                  gap: "0.94rem",
-                }}
+          <section className={classes.headerContainer}>
+            <h3 className={classes.welcomeText}>
+              <small className={classes.welcomeTextSmall}>
+                Hi,{" "}
+                {auth.currentUser?.displayName
+                  ? auth.currentUser?.displayName.split(" ")[0]
+                  : auth.currentUser?.email?.split("@")[0]}
+              </small>
+              What are you looking for today?
+            </h3>
+            <div className={classes.wrapper}>
+              <Input
+                id={"searchBar"}
+                type={"text"}
+                name={"Search headphone"}
+                onFocus={
+                  isDesktop ? () => undefined : () => navigate("/search")
+                }
+                onFieldChange={() => undefined}
               >
-                <SplideTrack className={classes.splideTrack}>
-                  {filteredData?.length !== 0 ? (
-                    filteredData?.map(
-                      (item, index) =>
-                        index < 6 && (
-                          <SplideSlide key={item.id}>
-                            <Banner
-                              key={item.id}
-                              title={item.name}
-                              id={item.id}
-                              category={item.category}
-                            />
-                          </SplideSlide>
-                        )
-                    )
-                  ) : (
-                    <li className={classes.noReturn}>
-                      <h2>Ops! No products found in this category.</h2>
-                      <IconShoppingBag
-                        width={"96"}
-                        height={"96"}
-                        color={"#BABABA"}
-                      />
-                    </li>
-                  )}
-                </SplideTrack>
-              </Splide>
-            )}
-            <div className={classes.showcaseText}>
-              <h4>Featured Products</h4>
-              <Link to="/products">See All</Link>
+                <SearchIcon size={"20"} color={"#BABABA"} />
+              </Input>
             </div>
-            {!data || loading ? (
-              <div className={classes.containerSkeleton}>
-                <CardSkeleton cards={2} />
+          </section>
+          {!isDesktop ? (
+            <section className={classes.showcase}>
+              <div className={classes.showcaseContainer}>
+                <Categories
+                  filterSelected={filterCategory}
+                  onChange={handleSelectChange}
+                  group={"category"}
+                />
+                {!data || loading ? (
+                  <BannerSkeleton />
+                ) : (
+                  <Carousel filteredData={filteredData} isBanner={true} />
+                )}
+                <Showcase
+                  title={"Featured Products"}
+                  condition={!data || loading}
+                  filteredData={filteredData}
+                />
               </div>
-            ) : error ? (
-              <p>{error}</p>
-            ) : (
-              <Splide
-                hasTrack={false}
-                options={{
-                  width: "100%",
-                  autoWidth: true,
-                  arrows: false,
-                  pagination: false,
-                  gap: "0.94rem",
-                }}
-              >
-                <SplideTrack className={classes.splideTrack}>
-                  {filteredData?.length !== 0 ? (
-                    filteredData?.map(
-                      (item, index) =>
-                        index < 8 && (
-                          <SplideSlide key={item.id}>
-                            <Card
-                              id={item.id}
-                              key={item.id}
-                              name={item.name}
-                              price={item.price}
-                              rating={item.rating}
-                              showReview={false}
-                              category={item.category}
-                            />
-                          </SplideSlide>
-                        )
-                    )
-                  ) : (
-                    <li className={classes.noReturn}>
-                      <h2>Ops! No products found in this category.</h2>
-                      <IconShoppingBag
-                        width={"96"}
-                        height={"96"}
-                        color={"#BABABA"}
-                      />
-                    </li>
-                  )}
-                </SplideTrack>
-              </Splide>
-            )}
-          </div>
-        </section>
-      </div>
+            </section>
+          ) : (
+            <>
+              <div className={classes.wrapperCategories}>
+                <Categories
+                  filterSelected={filterCategory}
+                  onChange={handleSelectChange}
+                  group={"category"}
+                />
+              </div>
+              {!data || loading ? (
+                <section className={classes.carouselBanners}>
+                  <BannerSkeleton />
+                </section>
+              ) : (
+                <section className={classes.carouselBanners}>
+                  <Carousel filteredData={filteredData} isBanner={true} />
+                </section>
+              )}
+              <section className={classes.carouselCards}>
+                <Showcase
+                  title={"Featured Products"}
+                  condition={!data || loading}
+                  filteredData={filteredData}
+                />
+              </section>
+            </>
+          )}
+        </div>
+      )}
     </>
   );
 }
-
-export default Home
