6a0cea47c34a6c5c863b94497adac5f76b1160bd
4ndferreira <andferreira.art@outlook.com>
2023-11-01 18:11:22 -0300

    Refactor: adjustments to conditional rendering

--- my-challenge-app/src/layouts/PrivateWrapper.tsx
+++ my-challenge-app/src/layouts/PrivateWrapper.tsx
@@ -1,22 +1,31 @@
 //React
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 //React Router Dom
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 //Firebase
 import { auth } from "../firebase/Config";
+import { onAuthStateChanged } from "firebase/auth";
 //Component
 import Loader from "../components/loader/Loader";
 
 export default function PrivateWrapper() {
-  const navigate = useNavigate();
-  const user = auth.currentUser;
-  const isLoginPage = location.pathname === "/";
+  const location = useLocation();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
   useEffect(() => {
-    if (!user) {
-      !isLoginPage && navigate("/", { state: { isPush: true } });
-    }
-  }, [isLoginPage, navigate, user]);
+    const unsubscribe = onAuthStateChanged(auth, (user) =>
+      setIsAuthenticated(user ? true : false)
+    );
+    return () => unsubscribe();
+  }, []);
 
-  return user || isLoginPage ? <Outlet /> : <Loader />
+  if (isAuthenticated === null) {
+    return <Loader />;
+  } else {
+    return isAuthenticated ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/login" state={{ from: location }} replace />
+    );
+  }
 }
