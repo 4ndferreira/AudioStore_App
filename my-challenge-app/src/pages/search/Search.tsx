890b185c746649011872a57abacac6068b85f21d
4ndferreira <andferreira.art@outlook.com>
2023-11-01 18:01:14 -0300

    Feat: added loading skeleton

--- my-challenge-app/src/pages/search/Search.tsx
+++ my-challenge-app/src/pages/search/Search.tsx
@@ -1,62 +1,53 @@
 //React
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 //Hook
 import { useFetch } from "../../hooks/useFetch";
 //Components
-import NavBar from "../../components/navBar/NavBar";
-import SearchItem from "../../components/searchItem/SearchItem"
+import HeaderMobileWhileBrowsing from "../../components/headerMobileWhileBrowsing/HeaderMobileWhileBrowsing";
+import CardForCartItemsOrSearchItems from "../../components/cardForCartItemsOrSearchItems/CardForCartItemsOrSearchItems";
+import CardSkeletonForCartItemsOrSearchItems from "../../components/cardSkeletonForCartItemsOrSearchItems/cardSkeletonForCartItemsOrSearchItems";
 import Input from "../../components/input/Input";
 //Icons
 import SearchIcon from "../../components/icons/SearchIcon";
 import IconShoppingBag from "../../components/icons/IconShoppingBag";
 //CSS
-import classes from "./Search.module.css"
+import classes from "./Search.module.css";
 
 export default function Search() {
-  const [value, setValue ] = useState ('')
-  const { data } = useFetch();
+  const [value, setValue] = useState("");
+  const { data, loading } = useFetch();
 
-  const filteredData = data?.filter((item) => 
+  const filteredData = data?.filter((item) =>
     item.name.toLowerCase().includes(value.toLowerCase())
   );
 
-  const handleOnInput = (
-    e: ChangeEvent<HTMLInputElement>
-  ) => {
-    const newValue = e.target.value;
-    setValue(newValue);
-  };
+  const handleOnSearch = (newValue: string) => setValue(newValue);
 
   return (
     <>
+      <HeaderMobileWhileBrowsing />
       <div className={classes.container}>
-        <NavBar 
-          link="/home" 
-          title={"Search"} 
-          isShoppingCart={false} 
-        />
         <div className={classes.wrapper}>
           <Input
             id={"searchBar"}
             type={"text"}
             name={"Search headphone"}
-            element={<SearchIcon size={"20"} color={"#BABABA"} />}
-            value={value}
-            onInput={handleOnInput}
             onFocus={undefined}
-          />
+            onFieldChange={handleOnSearch}
+          >
+            <SearchIcon size={"20"} color={"#BABABA"} />
+          </Input>
         </div>
         {value ? (
           <ul className={classes.listItems}>
             {filteredData && filteredData.length > 0 ? (
               filteredData.map((item) => (
-                <SearchItem
+                <CardForCartItemsOrSearchItems
                   key={item.id}
                   name={item.name}
                   price={item.price}
                   rating={item.rating}
                   category={item.category}
-                  isShoppingCart={false}
                   itemId={item.id}
                   count={0}
                   showModal={() => null}
@@ -65,11 +56,7 @@ export default function Search() {
               ))
             ) : (
               <div className={classes.textAlert}>
-                <IconShoppingBag 
-                  width={"72"} 
-                  height={"72"} 
-                  color={"#BABABA"} 
-                />
+                <IconShoppingBag width={"72"} height={"72"} color={"#BABABA"} />
                 <p className={classes.text}>Sorry, No Products Found</p>
               </div>
             )}
@@ -78,25 +65,30 @@ export default function Search() {
           <>
             <h2 className={classes.text}>Popular Product</h2>
             <ul className={classes.listItems}>
-              {filteredData
-                ?.sort((a, b) => b.reviews.length - a.reviews.length)
-                .map(
-                  (item, index) =>
-                    index < 5 && (
-                      <SearchItem
-                        key={item.id}
-                        name={item.name}
-                        price={item.price}
-                        rating={item.rating}
-                        category={item.category}
-                        isShoppingCart={false}
-                        itemId={item.id}
-                        count={0}
-                        showModal={() => null}
-                        onItemDelete={() => null}
-                      />
-                    )
-                )}
+              {!data || loading ? (
+                <div className={classes.containerSkeleton}>
+                  <CardSkeletonForCartItemsOrSearchItems cards={4} />
+                </div>
+              ) : (
+                filteredData
+                  ?.sort((a, b) => b.reviews.length - a.reviews.length)
+                  .map(
+                    (item, index) =>
+                      index < 5 && (
+                        <CardForCartItemsOrSearchItems
+                          key={item.id}
+                          name={item.name}
+                          price={item.price}
+                          rating={item.rating}
+                          category={item.category}
+                          itemId={item.id}
+                          count={0}
+                          showModal={() => null}
+                          onItemDelete={() => null}
+                        />
+                      )
+                  )
+              )}
             </ul>
           </>
         )}
