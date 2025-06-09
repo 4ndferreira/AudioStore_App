17f253669e308a3762889e9c730cf73da810be41
Anderson Ferreira <anderson.ferreira@compasso.com.br>
2025-06-09 09:24:38 -0300

    Refactor: stylesheet changed to scss and created interface for props

--- src/components/allFilteringOptions/AllFilteringOptions.tsx
+++ src/components/allFilteringOptions/AllFilteringOptions.tsx
@@ -2,50 +2,54 @@
 import { MouseEventHandler, ChangeEventHandler } from "react";
 //Components
 import Button from "../button/Button";
-import Categories from "../listOfSelectorLabels/ListOfSelectorLabels";
+import ListOfSelectorLabels from "../listOfSelectorLabels/ListOfSelectorLabels";
 //Icons
 import IconClose from "../icons/IconClose";
 //CSS
-import classes from "./AllFilteringOptions.module.css";
+import styles from "./AllFilteringOptions.module.scss";
 
-export default function AllFilteringOptions(props: {
-  handleCloseFilter: MouseEventHandler<HTMLButtonElement> | undefined;
+export interface AllFilteringOptionsProps {
   filterCategory: string;
-  handleSelectCategory: ChangeEventHandler<HTMLInputElement>;
   selectSortBy: string;
-  handleSelectSortBy: ChangeEventHandler<HTMLInputElement>;
   handleSortBy: MouseEventHandler<HTMLButtonElement>;
-}) {
+  handleCloseFilter: MouseEventHandler<HTMLButtonElement>;
+  handleSelectCategory: ChangeEventHandler<HTMLInputElement>;
+  handleSelectSortBy: ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function AllFilteringOptions({
+  filterCategory,
+  selectSortBy,
+  handleSortBy,
+  handleCloseFilter,
+  handleSelectCategory,
+  handleSelectSortBy,
+}: AllFilteringOptionsProps) {
   const nameOfFilter = ["Category", "Sort By"];
 
   return (
-    <form className={classes.filterContainer}>
-      <div className={classes.filterTitle}>
+    <form className={styles.filterContainer}>
+      <div className={styles.filterTitle}>
         <h2>Filter</h2>
-        <button
-          className={classes.closeButton}
-          onClick={props.handleCloseFilter}
-        >
+        <button className={styles.closeButton} onClick={handleCloseFilter}>
           <IconClose color={"black"} />
         </button>
       </div>
       {nameOfFilter.map((filter, index) => (
         <div key={index}>
           <h4>{filter}</h4>
-          <Categories
+          <ListOfSelectorLabels
             filterSelected={
-              filter === "Category" ? props.filterCategory : props.selectSortBy
+              filter === "Category" ? filterCategory : selectSortBy
             }
             onChange={
-              filter === "Category"
-                ? props.handleSelectCategory
-                : props.handleSelectSortBy
+              filter === "Category" ? handleSelectCategory : handleSelectSortBy
             }
             group={filter.toLowerCase().replace(" ", "-")}
           />
         </div>
       ))}
-      <Button type={"submit"} onClick={props.handleSortBy}>
+      <Button type={"submit"} onClick={handleSortBy}>
         Apply Filter
       </Button>
     </form>
---
+++ src/components/allFilteringOptions/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./AllFilteringOptions"
+export * from "./AllFilteringOptions"
